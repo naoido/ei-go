ec1eb5243b362698b8ef77d055220566465d53c5
naoto.kido <naoto.kido@techblue.style>
2025-02-26 17:45:58 +0900

    ゲーム画面の追加

---
+++ web/generate.py
@@ -0,0 +1,16 @@
+import requests
+
+text = "The answer is."
+lang = "en"
+
+url = f"https://translate.google.com/translate_tts?ie=UTF-8&tl={lang}&q={text}&client=tw-ob"
+
+headers = {
+    "User-Agent": "Mozilla/5.0"
+}
+response = requests.get(url, headers=headers)
+
+with open("google_translate_tts.mp3", "wb") as f:
+    f.write(response.content)
+
+print("音声ファイル 'google_translate_tts.mp3' を作成しました。")
--- web/src/pages/room/[id].vue
+++ web/src/pages/room/[id].vue
@@ -1,12 +1,47 @@
 <template>
     <Suspense>
-        <Standby :roomId="id"/>
+        <Standby v-if="!isPlaying" :roomId="id" :users="users" @play="start" @append="append" />
+        <Playing v-else :users="users" :answer="answer" @setAnswer="setAnswer" />
     </Suspense>
 </template>
 
 <script setup lang="ts">
+import { ref } from 'vue';
 import { useRoute } from 'vue-router';
 
 const route = useRoute();
 const id = route.params.id;
+
+type User = {
+    username: string,
+    isReady: boolean,
+}
+
+type Answer = {
+    username: string,
+    text: string
+}
+
+const hogeAnswer = {
+    username: "naoido",
+    text: "If you're making an iOS app, what language do you use?"
+}
+
+const answer = ref<Answer | null>(hogeAnswer);
+const setAnswer = (value: Answer | null) => answer.value = value;
+
+const users = ref<User[]>([
+    {username: "thirdlf", isReady: true},
+    {username: "naoido", isReady: true},
+    {username: "ei", isReady: true},
+]);
+
+const append = () => {
+    users.value.push({username: "ei", isReady: true})
+}
+
+const isPlaying = ref(false);
+const start = () => {
+    isPlaying.value = true;
+}
 </script>
