161430a95818bc2ebaed51d5cf685795b007ce48
suki-9 <suki.music2004@gmail.com>
2025-02-27 02:40:09 +0900

    feat(module::json_utils): ハンドリングを追加

--- backend/module/json_utils.cc
+++ backend/module/json_utils.cc
@@ -1,22 +1,28 @@
 #include "json_utils.h"
 
-Json::Value* json_parse(Json::Value &root, const std::string &message) {
-  try {
+Json::Value *json_parse(Json::Value &root, const std::string &message)
+{
+  try
+  {
     Json::CharReaderBuilder builder;
     builder["collectComments"] = true;
     JSONCPP_STRING errs;
     auto reader = builder.newCharReader();
-    if (!reader->parse(message.c_str(), message.cend().base(), &root, &errs)) {
+    if (!reader->parse(message.c_str(), message.cend().base(), &root, &errs))
+    {
       throw "TODO Err";
     }
     return &root;
-  } catch (...) {
+  }
+  catch (...)
+  {
+    ERR_LOGGER("Json", "purse failure");
     throw "TODO Err";
   }
 }
 
-std::string json_stringify(Json::Value &root) {
-  Json::StreamWriterBuilder builder;
-  builder["indentation"] = "";
-  return Json::writeString(builder, root);
+std::string json_stringify(Json::Value &json_value)
+{
+  Json::FastWriter fastWriter;
+  return fastWriter.write(json_value);
 };
