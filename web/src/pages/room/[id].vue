7f75b398484c6d7325fdd8e78b77dd9c4a1f2758
naoto.kido <naoto.kido@techblue.style>
2025-02-26 15:42:32 +0900

    web_uiのベースを作成

---
+++ web/src/pages/room/[id].vue
@@ -0,0 +1,12 @@
+<template>
+    <Suspense>
+        <Standby :roomId="id"/>
+    </Suspense>
+</template>
+
+<script setup lang="ts">
+import { useRoute } from 'vue-router';
+
+const route = useRoute();
+const id = route.params.id;
+</script>
